5c6712f9bf9db97d4bf26b6ad6d4f1cdac89d5d7
Mike Boyer <boyer.mike.e@gmail.com>
2017-07-09 21:36:31 -0400

    Throwable Blocks

---
+++ Assets/HandInteraction.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandInteraction : MonoBehaviour {
+    public SteamVR_TrackedObject trackedObj;
+    private SteamVR_Controller.Device device;
+    public float throwForce = 1.5f;
+
+	// Use this for initialization
+	void Start () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        device = SteamVR_Controller.Input((int)trackedObj.index);
+	}
+
+    private void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.CompareTag("Throwable"))
+        {
+            if (device.GetPressUp(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                ThrowObject(col);
+            }
+            else if(device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            {
+                GrabObject(col);
+            }
+        }
+    }
+    void GrabObject(Collider coli)
+    {
+        //make controller its parent
+        coli.transform.SetParent(gameObject.transform);
+        //turn off physics
+        coli.GetComponent<RigidBody>().isKinematic = true;
+        //vibrate controller
+        device.TriggerHapticPulse(2000);
+        Debug.Log("Object grabbed!");
+    }
+    void ThrowObject(Collider coli)
+    {
+        //unparent controller
+        coli.transform.SetParent(null);
+        Rigidbody rb = coli.GetComponent<Rigidbody>();
+        //turn on physics
+        rb.isKinematic = false;
+        //set velocity based on controller movement
+        rb.velocity = device.velocity * throwForce;
+        rb.angularVelocity = device.angularVelocity;
+        Debug.Log("Object thrown!");
+    }
+}
