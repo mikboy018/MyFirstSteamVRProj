ac8dc4f3088f31eabda9abc34f94baa3889487a6
Mike Boyer <boyer.mike.e@gmail.com>
2017-07-10 20:44:58 -0400

    Object Menu added

--- Assets/HandInteraction.cs
+++ Assets/HandInteraction.cs
@@ -2,20 +2,71 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandInteraction : MonoBehaviour {
+public class HandInteraction : MonoBehaviour
+{
     public SteamVR_TrackedObject trackedObj;
     private SteamVR_Controller.Device device;
     public float throwForce = 1.5f;
 
-	// Use this for initialization
-	void Start () {
+    //Swipe
+    public float swipeSum;
+    public float touchLast;
+    public float touchCurrent;
+    public float distance;
+    public bool hasSwipedLeft;
+    public bool hasSwipedRight;
+    public ObjectMenuManager objectMenuManager;
+
+    // Use this for initialization
+    void Start()
+    {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         device = SteamVR_Controller.Input((int)trackedObj.index);
-	}
+        if (device.GetTouchDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            touchLast = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+        }
+        if (device.GetTouch(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            touchCurrent = device.GetAxis(Valve.VR.EVRButtonId.k_EButton_SteamVR_Touchpad).x;
+            distance = touchCurrent - touchLast;
+            touchLast = touchCurrent;
+            swipeSum += distance;
+            if (swipeSum > 0.5f & !hasSwipedRight)
+            {
+                swipeSum = 0;
+                SwipeRight();
+                hasSwipedRight = true;
+                hasSwipedLeft = false;
+            }
+            if (swipeSum < -0.5f & !hasSwipedLeft)
+            {
+                swipeSum = 0;
+                SwipeLeft();
+                hasSwipedRight = false;
+                hasSwipedLeft = true;
+            }
+        }
+        if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            swipeSum = 0;
+            touchCurrent = 0;
+            touchLast = 0;
+            hasSwipedLeft = false;
+            hasSwipedRight = false;
+
+        }
+        if (device.GetPressDown(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            //Spawn object currently selected by menu
+            SpawnObject();
+        }
+    }
 
     private void OnTriggerStay(Collider col)
     {
@@ -25,7 +76,7 @@ public class HandInteraction : MonoBehaviour {
             {
                 ThrowObject(col);
             }
-            else if(device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
+            else if (device.GetPressDown(SteamVR_Controller.ButtonMask.Trigger))
             {
                 GrabObject(col);
             }
@@ -53,4 +104,21 @@ public class HandInteraction : MonoBehaviour {
         rb.angularVelocity = device.angularVelocity;
         Debug.Log("Object thrown!");
     }
+
+    public void SwipeLeft()
+    {
+        objectMenuManager.MenuLeft();
+        Debug.Log("Swiped left!");
+    }
+    public void SwipeRight()
+    {
+        objectMenuManager.MenuRight();
+        Debug.Log("Swiped right!");
+    }
+
+    void SpawnObject()
+    {
+        objectMenuManager.SpawnCurrentObject();
+    }
 }
+    
